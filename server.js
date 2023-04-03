7fc445af7c5b8788174ee5add7c7663657108711
Leo Wei <leowei727@yahoo.ca>
2023-04-02 21:31:31 -0600

    finished web app

--- server.js
+++ server.js
@@ -4,12 +4,56 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/fake/people', (req, res) => {
-  res.sendFile(__dirname + '/public/fake_entity_person.html')
+  res.sendFile(__dirname + '/public/fake_entity_people.html')
+})
+
+app.get('/fake/gpe', (req, res) => {
+  res.sendFile(__dirname + '/public/fake_entity_gpe.html')
 })
 
 app.get('/fake/locations', (req, res) => {
   res.sendFile(__dirname + '/public/fake_entity_locations.html')
 })
+
+app.get('/fake', (req, res) => {
+  res.sendFile(__dirname + '/public/fake.html')
+})
+
+
+app.get('/true/people', (req, res) => {
+  res.sendFile(__dirname + '/public/true_entity_people.html')
+})
+
+app.get('/true/gpe', (req, res) => {
+  res.sendFile(__dirname + '/public/true_entity_gpe.html')
+})
+
+app.get('/true/locations', (req, res) => {
+  res.sendFile(__dirname + '/public/true_entity_locations.html')
+})
+
+app.get('/true', (req, res) => {
+  res.sendFile(__dirname + '/public/true.html')
+})
+
+app.get('/russia/gpe', (req, res) => {
+  res.sendFile(__dirname + '/public/russia_gpe.html')
+})
+
+app.get('/russia/people', (req, res) => {
+  res.sendFile(__dirname + '/public/russia_people.html')
+})
+
+
+app.get('/russia/locations', (req, res) => {
+  res.sendFile(__dirname + '/public/russia_locations.html')
+})
+
+
+app.get('/russia', (req, res) => {
+  res.sendFile(__dirname + '/public/russia.html')
+})
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
